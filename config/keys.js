8f86f31f80371388b29578dcb87314da8201c2f3
Tom Hodgkinson <hdgknsn@gmail.com>
2019-07-21 20:51:28 -0400

    completed user model, registration, login w/user auth, profile model

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: "mongodb://hdgknsn:lino7196@ds133932.mlab.com:33932/devconnector"
+  mongoURI: "mongodb://hdgknsn:lino7196@ds133932.mlab.com:33932/devconnector",
+  secretOrKey: "secret"
 };
 
 // mongo ds133932.mlab.com:33932/devconnector -u hdgknsn -p lino7196
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
+// Load Profile Model
+const User = require("../../models/User");
+// Load User Model
+const Profile = require("../../models/Profile");
 // @route GET api/profile/test
 // @desc Tests profile route
 // @access Public
 router.get("/test", (req, res) => res.json({ message: "Profile Works" }));
+
+// @route GET api/profile
+// @desc Get current user profile
+// @access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(400).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
