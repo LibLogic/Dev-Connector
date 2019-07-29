b9210531a6b08fe8dc168445cff4c933bb058727
Tom Hodgkinson <hdgknsn@gmail.com>
2019-07-28 23:05:40 -0400

    working on the Login component

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
+import store from "./store";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+import "./App.css";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
