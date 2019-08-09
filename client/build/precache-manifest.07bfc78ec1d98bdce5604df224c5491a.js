dc8fa267fb53769aece6ae1b1ab6678cf44694f7
Tom Hodgkinson <hdgknsn@gmail.com>
2019-08-09 10:55:47 -0400

    removed /build from .gitignore file

---
+++ client/build/precache-manifest.07bfc78ec1d98bdce5604df224c5491a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06eec0b3b57f2088422e9261f134ac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1439dd6146048f3c3083",
+    "url": "/static/css/main.c2f16710.chunk.css"
+  },
+  {
+    "revision": "526884a17690159b90ed",
+    "url": "/static/js/2.b66c59df.chunk.js"
+  },
+  {
+    "revision": "1439dd6146048f3c3083",
+    "url": "/static/js/main.87814bdf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
