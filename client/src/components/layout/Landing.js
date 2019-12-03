70e94a5782d9dc816304e546dc56bb83c7efd929
Tom Hodgkinson <hdgknsn@gmail.com>
2019-12-02 22:54:18 -0500

    Update Landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,7 +17,7 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">LibLogic Connector</h1>
+                <h1 className="display-3 mb-4">LibLogic DevConnector</h1>
                 <p className="lead">
                   {" "}
                   Create — Link developer profiles/portfolios, share posts and get
